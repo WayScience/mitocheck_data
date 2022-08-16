f72f30117b0bc6aae504b66ae1d9a45ef2cba724
roshankern <roshan.kern@ucdenver.edu>
2022-08-16 15:54:39 -0600

    delete unecessary files

--- 1.preprocess_data/1.preprocess_training_movies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-jupyter nbconvert --to python preprocess_training_data.ipynb
-python preprocess_training_data.py
--- 2.segment_nuclei/2.segment_training_nuclei.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-jupyter nbconvert --to python segment_training_data.ipynb
-python segment_training_data.py
