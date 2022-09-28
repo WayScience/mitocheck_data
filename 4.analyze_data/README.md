bbfc3ae7e64bcef9b2d39a6eb9a36e20891744ea
roshankern <roshan.kern@ucdenver.edu>
2022-09-28 15:09:13 -0600

    update docs

--- 4.analyze_data/README.md
+++ 4.analyze_data/README.md
@@ -7,10 +7,14 @@ In this module, we analyze the normalized training data features from [3.normali
 We use [UMAP](https://github.com/lmcinnes/umap) for analyis of features.
 UMAP was introduced in [McInnes, L, Healy, J, 2018](https://arxiv.org/abs/1802.03426) as a manifold learning technique for dimension reduction.
 We use UMAP to reduce the feature data from 1280 features to 1, 2, and 3 dimensions.
+We use [Matplotlib](https://matplotlib.org/) to visualize the 1D, 2D, and 3D UMAPS.
 
-We use [Matplotlib](https://matplotlib.org/) and [seaborn](https://seaborn.pydata.org/) for data visualization.
+For each reduction with UMAP, we create two types of visualizations.
+The first visualization colors all points by their phenotypic class.
+The second visualization colors points for only certain phenotypic classes, with all other phenotypic classes being colored gray.
 
-**Note:** Phenotypic classes used for analysis can be changed with the `classes_to_keep` variable in [analyze_data.ipynb](analyze_data.ipynb).
+**Note:** Phenotypic classes colored in second visualization can be changed with the `classes_2 = [
+` variable in [analyze_data.ipynb](analyze_data.ipynb).
 
 ## Step 1: Analyze Data
 
--- 4.analyze_data/scripts/nbconverted/analyze_data.py
+++ 4.analyze_data/scripts/nbconverted/analyze_data.py
@@ -8,31 +8,70 @@
 
 import pathlib
 import pandas as pd
+import seaborn as sns
+from matplotlib.colors import rgb2hex
 
 import sys
 sys.path.append("../utils")
 from load_utils import split_data
-from analysis_utils import get_2D_umap_embeddings, show_1D_umap, show_2D_umap, show_3D_umap
+from analysis_utils import get_class_colors, show_1D_umap, show_2D_umap, show_3D_umap
 
 
-# ### Load training data, see class counts
+# ### Load training data
 
 # In[2]:
 
 
 training_data_path = pathlib.Path("../3.normalize_data/normalized_data/training_data.csv.gz")
 training_data = pd.read_csv(training_data_path, compression="gzip", index_col=0)
-training_data["Mitocheck_Phenotypic_Class"].value_counts()
+
+metadata_dataframe, feature_data = split_data(training_data)
+phenotypic_classes = metadata_dataframe["Mitocheck_Phenotypic_Class"]
+
+training_data
 
 
-# ### Only UMAP certain phenotypic classes
+# ### Set UMAP display settings, save directory
 
 # In[3]:
 
 
-classes_to_keep = [
+point_size = 25
+alpha = 0.6
+color_palette = "bright"
+
+save_dir = pathlib.Path("umaps/")
+save_dir.mkdir(exist_ok=True, parents=True)
+
+
+# ### Set class colors
+
+# In[4]:
+
+
+classes_1 = [
     "Polylobed",
     "Binuclear",
+    "Grape",
+    "Interphase",
+    "Prometaphase",
+    "Artefact",
+    "Apoptosis",
+    "ADCCM",
+    "MetaphaseAlignment",
+    "SmallIrregular",
+    "Hole",
+    "Large",
+    "Anaphase",
+    "Metaphase",
+    "UndefinedCondensed",
+    "Elongated",
+    "Folded",
+]
+
+classes_2 = [
+    # "Polylobed",
+    # "Binuclear",
     "Grape",
     "Interphase",
     "Prometaphase",
@@ -50,26 +89,10 @@ classes_to_keep = [
     # "Folded",
 ]
 
-training_data = training_data.loc[
-    training_data["Mitocheck_Phenotypic_Class"].isin(classes_to_keep)
-]
-metadata_dataframe, feature_data = split_data(training_data)
-phenotypic_classes = metadata_dataframe["Mitocheck_Phenotypic_Class"]
+class_colors_1 = get_class_colors(classes_1, "rainbow")
+class_colors_2 = get_class_colors(classes_2, "bright")
 
-training_data
-
-
-# ### Set UMAP display settings, save directory
-
-# In[4]:
-
-
-point_size = 25
-alpha = 0.6
-color_palette = "bright"
-
-save_dir = pathlib.Path("umaps/")
-save_dir.mkdir(exist_ok=True, parents=True)
+class_colors_2
 
 
 # ### 1D UMAP
@@ -78,24 +101,44 @@ save_dir.mkdir(exist_ok=True, parents=True)
 
 
 umap_1D_save_path = pathlib.Path(f"{save_dir}/norm_train_1D_umap.png")
-show_1D_umap(feature_data, phenotypic_classes, save_path=umap_1D_save_path, point_size=point_size, alpha=alpha, palette=color_palette)
+show_1D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_1D_save_path, point_size=point_size, alpha=alpha)
 
-
-# ### 2D UMAP
 
 # In[6]:
 
 
-umap_2D_save_path = pathlib.Path(f"{save_dir}/norm_train_2D_umap.png")
-x_data, y_data = get_2D_umap_embeddings(feature_data)
-show_2D_umap(x_data, y_data, phenotypic_classes, save_path=umap_2D_save_path, point_size=point_size, alpha=alpha, palette=color_palette)
+umap_1D_save_path = pathlib.Path(f"{save_dir}/norm_train_1D_umap_other.png")
+show_1D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_1D_save_path, point_size=point_size, alpha=alpha)
 
 
-# ### 3D UMAP
+# ### 2D UMAP
 
 # In[7]:
 
 
+umap_2D_save_path = pathlib.Path(f"{save_dir}/norm_train_2D_umap.png")
+show_2D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_2D_save_path, point_size=point_size, alpha=alpha)
+
+
+# In[8]:
+
+
+umap_2D_save_path = pathlib.Path(f"{save_dir}/norm_train_2D_umap_other.png")
+show_2D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_2D_save_path, point_size=point_size, alpha=alpha)
+
+
+# ### 3D UMAP
+
+# In[9]:
+
+
 umap_3D_save_path = pathlib.Path(f"{save_dir}/norm_train_3D_umap.png")
-show_3D_umap(feature_data, phenotypic_classes, save_path=umap_3D_save_path, point_size=point_size, alpha=alpha, palette=color_palette)
+show_3D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_3D_save_path, point_size=point_size, alpha=alpha)
+
+
+# In[10]:
+
+
+umap_3D_save_path = pathlib.Path(f"{save_dir}/norm_train_3D_umap_other.png")
+show_3D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_3D_save_path, point_size=point_size, alpha=alpha)
 
