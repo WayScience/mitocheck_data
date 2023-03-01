2a6a09508a63b522d2a301840ef104b56257bdbf
roshankern <roshan.kern@ucdenver.edu>
2023-03-01 15:32:35 -0700

    remove unecessary create folder line

--- 1.idr_streams/idr_streams.sh
+++ 1.idr_streams/idr_streams.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Go into streams/
 cd streams/
-# Make log folder if it does not already exist
-mkdir -p logs
 # Run all streams in streams/
 for python_file in *.py; do python "$python_file"; done
