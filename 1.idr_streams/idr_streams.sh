74a36f69d6d99bc86e75b4cb400f315ff7dcea8e
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-03-02 14:58:41 -0500

    Idr stream module refactor (#26)
    * Co-authored-by: Jenna Tomkinson <jenna.tomkinson@ucdenver.edu>
    
    * add files for CP run
    
    * remove run cellpose plugin
    
    * refactor idrstreams
    
    * create python merge files
    
    * remove unecessary create folder line
    
    * docs
    
    * change conda environments with bash file
    
    * black formatting

--- 1.idr_streams/idr_streams.sh
+++ 1.idr_streams/idr_streams.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-# Go into streams/
+
+# go into streams/
 cd streams/
-# Make log folder if it does not already exist
-mkdir -p logs
-# Run all streams in streams/
-for python_file in *.py; do python "$python_file"; done
+
+# activate conda environment for idrstream cp
+conda activate idrstream_cp
+# extract cp features
+python cp_streams.py
+
+# activate conda environment for idrstream dp
+conda activate idrstream_dp
+# extract cp features
+python dp_streams.py
+
+# merge features
+python merge_streams.py
