3d602c09e7cb527da3fd0108ab50720d65767e23
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-03-09 16:07:28 -0500

    Refactor umaps module (#30)
    * rerun notebook
    
    * docs, convert notebook
    
    * Update 4.analyze_data/README.md
    
    Co-authored-by: Erik Serrano <31600622+axiomcura@users.noreply.github.com>
    
    * documentation
    
    ---------
    
    Co-authored-by: Erik Serrano <31600622+axiomcura@users.noreply.github.com>

--- 4.analyze_data/scripts/nbconverted/analyze_data.py
+++ 4.analyze_data/scripts/nbconverted/analyze_data.py
@@ -25,13 +25,10 @@ from analysis_utils import get_class_colors, show_1D_umap, show_2D_umap, show_3D
 training_data_path = pathlib.Path("../3.normalize_data/normalized_data/training_data.csv.gz")
 training_data = pd.read_csv(training_data_path, compression="gzip", index_col=0)
 
-metadata_dataframe, feature_data = split_data(training_data)
-phenotypic_classes = metadata_dataframe["Mitocheck_Phenotypic_Class"]
-
 training_data
 
 
-# ### Set UMAP display settings, save directory
+# ### Set UMAP display settings, save directory, class colors
 
 # In[3]:
 
@@ -39,21 +36,6 @@ training_data
 point_size = 25
 alpha = 0.6
 color_palette = "bright"
-
-save_dir = pathlib.Path("umaps/")
-save_dir.mkdir(exist_ok=True, parents=True)
-
-
-# ### Set class colors
-
-# In[4]:
-
-
-training_data["Mitocheck_Phenotypic_Class"].unique().tolist()
-
-
-# In[5]:
-
 
 classes_1 = [
     "Large",
@@ -74,6 +56,9 @@ classes_1 = [
     "OutOfFocus",
 ]
 
+# classes that aren't commented out will get a color for their particular class
+# those that are commented out will be colored gray and labeled "other"
+# in the second set of umaps
 classes_2 = [
     # "Large",
     "Prometaphase",
@@ -96,53 +81,30 @@ classes_2 = [
 class_colors_1 = get_class_colors(classes_1, "rainbow")
 class_colors_2 = get_class_colors(classes_2, "bright")
 
-class_colors_2
+
+# In[4]:
 
 
-# ### 1D UMAP
+feature_types = ["CP", "DP", "CP_and_DP"]
 
-# In[6]:
-
-
-umap_1D_save_path = pathlib.Path(f"{save_dir}/norm_train_1D_umap.png")
-show_1D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_1D_save_path, point_size=point_size, alpha=alpha)
-
-
-# In[7]:
-
-
-umap_1D_save_path = pathlib.Path(f"{save_dir}/norm_train_1D_umap_other.png")
-show_1D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_1D_save_path, point_size=point_size, alpha=alpha)
-
-
-# ### 2D UMAP
-
-# In[8]:
-
-
-umap_2D_save_path = pathlib.Path(f"{save_dir}/norm_train_2D_umap.png")
-show_2D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_2D_save_path, point_size=point_size, alpha=alpha)
-
-
-# In[9]:
-
-
-umap_2D_save_path = pathlib.Path(f"{save_dir}/norm_train_2D_umap_other.png")
-show_2D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_2D_save_path, point_size=point_size, alpha=alpha)
-
-
-# ### 3D UMAP
-
-# In[10]:
-
-
-umap_3D_save_path = pathlib.Path(f"{save_dir}/norm_train_3D_umap.png")
-show_3D_umap(feature_data, phenotypic_classes, class_colors_1, save_path=umap_3D_save_path, point_size=point_size, alpha=alpha)
-
-
-# In[11]:
-
-
-umap_3D_save_path = pathlib.Path(f"{save_dir}/norm_train_3D_umap_other.png")
-show_3D_umap(feature_data, phenotypic_classes, class_colors_2, save_path=umap_3D_save_path, point_size=point_size, alpha=alpha)
+for feature_type in feature_types:
+    print(f"Showing UMAPs created with {feature_type} features")
+    
+    # the trainind data dataframe is split into two components:
+    # metadata: info about the cell including its labeled phenotypic class, location, perturbation, etc
+    # feature data: the CP, DP, or merged features for each cell
+    metadata_dataframe, feature_data = split_data(training_data, feature_type)
+    phenotypic_classes = metadata_dataframe["Mitocheck_Phenotypic_Class"]
+    
+    # show 1D umaps
+    # class colors 1 - all classes included
+    show_1D_umap(feature_data, phenotypic_classes, class_colors_1, point_size=point_size, alpha=alpha)
+    # class colors 2 - only certain classes included
+    show_1D_umap(feature_data, phenotypic_classes, class_colors_2, point_size=point_size, alpha=alpha)
+    
+    # show 2D umaps
+    # class colors 1 - all classes included
+    show_2D_umap(feature_data, phenotypic_classes, class_colors_1, point_size=point_size, alpha=alpha)
+    # class colors 2 - only certain classes included
+    show_2D_umap(feature_data, phenotypic_classes, class_colors_2, point_size=point_size, alpha=alpha)
 
