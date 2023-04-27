7202707760554892ef3af1ca708d8713ecd4ed68
roshankern <roshan.kern@ucdenver.edu>
2023-04-27 14:41:29 -0600

    rerun format module

--- 2.format_training_data/format_training_data.sh
+++ 2.format_training_data/format_training_data.sh
@@ -2,4 +2,4 @@
 # Convert notebook to python file and execute
 jupyter nbconvert --to python \
         --FilesWriter.build_directory=scripts/nbconverted \
-        --execute training_data.ipynb
+        --execute format_training_data.ipynb
