f8ad5e5dcba88f2f71062a579ba06a056110fa69
roshankern <roshan.kern@ucdenver.edu>
2024-01-10 17:59:14 -0700

    rerun training data for ic

--- 3.normalize_data/normalize_data.py
+++ 3.normalize_data/normalize_data.py
@@ -7,34 +7,34 @@ from joblib import dump
 import sys
 
 sys.path.append("../utils")
-from load_utils import compile_mitocheck_batch_data, split_data
+from load_utils import compile_mitocheck_batch_data
 from normalization_utils import get_normalization_scaler, get_normalized_mitocheck_data
 
 # get normalization scaler from negative control features (normalization population)
 print("Getting normalization scaler...")
 negative_control_data_path = pathlib.Path(
-    "../1.idr_streams/extracted_features/negative_control_data/merged_features/"
+    "../1.idr_streams/extracted_features__no_ic/negative_control_data/merged_features/"
 )
 normalization_scaler = get_normalization_scaler(negative_control_data_path)
 
 # save normalization scaler
-norm_scaler_save_path = pathlib.Path("scaler/normalization_scaler.joblib")
+norm_scaler_save_path = pathlib.Path("scaler__no_ic/normalization_scaler.joblib")
 norm_scaler_save_path.parent.mkdir(parents=True, exist_ok=True)
 dump(normalization_scaler, norm_scaler_save_path)
 
 # make results dir if it does not already exist
-results_dir = pathlib.Path("normalized_data/")
+results_dir = pathlib.Path("normalized_data__no_ic/")
 results_dir.mkdir(parents=True, exist_ok=True)
 
 # normalize the data at the following paths
 training_data_path = pathlib.Path(
-    "../2.format_training_data/results/training_data.csv.gz"
+    "../2.format_training_data/results__no_ic/training_data.csv.gz"
 )
 negative_control_data_path = pathlib.Path(
-    "../1.idr_streams/extracted_features/negative_control_data/merged_features/"
+    "../1.idr_streams/extracted_features__no_ic/negative_control_data/merged_features/"
 )
 positive_control_data_path = pathlib.Path(
-    "../1.idr_streams/extracted_features/positive_control_data/merged_features/"
+    "../1.idr_streams/extracted_features__no_ic/positive_control_data/merged_features/"
 )
 
 # normalize training data
