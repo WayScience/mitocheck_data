0bee9334c0365c06a7c77ec1638b729024237efa
roshankern <roshan.kern@ucdenver.edu>
2024-01-19 19:48:33 -0700

    update streams python

--- 1.idr_streams/streams/merge_streams.py
+++ 1.idr_streams/streams/merge_streams.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import pathlib
-
 import sys
+import itertools
+
+import pandas as pd
 
 sys.path.append("../IDR_stream/")
 from idrstream.merge_CP_DP import save_merged_CP_DP_run
@@ -9,13 +10,19 @@ from idrstream.merge_CP_DP import save_merged_CP_DP_run
 # path to directory with IDR_stream run outputs (training_data, negative_control_data, positive_control_data)
 extracted_features = pathlib.Path("../extracted_features/")
 
-for extracted_features_dir in sorted(extracted_features.iterdir()):
-    print(f"Merging streams for {extracted_features_dir.name}")
+data_types = ["negative_control_data", "positive_control_data", "training_data"]
+dataset_types = ["ic", "no_ic"]
+
+for data_type, dataset_type in itertools.product(data_types, dataset_types):
+    print(f"Merging streams for data type {data_type} and dataset type {dataset_type}")
+    
+    base_dir_path = pathlib.Path(f"{extracted_features}/{data_type}__{dataset_type}/")
+    print(base_dir_path)
 
     # specify paths to CP, DP and merged data
-    cp_data_dir_path = pathlib.Path(f"{extracted_features_dir}/CP_features/")
-    dp_data_dir_path = pathlib.Path(f"{extracted_features_dir}/DP_features/")
-    merged_data_dir_path = pathlib.Path(f"{extracted_features_dir}/merged_features/")
+    cp_data_dir_path = pathlib.Path(f"{base_dir_path}/CP_features")
+    dp_data_dir_path = pathlib.Path(f"{base_dir_path}/CP_features")
+    merged_data_dir_path = pathlib.Path(f"{base_dir_path}/merged_features")
 
     # merge CP and DP runs!
     save_merged_CP_DP_run(cp_data_dir_path, dp_data_dir_path, merged_data_dir_path)
